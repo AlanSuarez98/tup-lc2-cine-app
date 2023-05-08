517d0d3001557e8d747f84055cf6137a3cb988b1
Alan Suarez <alansuarez641@hotmail.com>
2023-05-07 23:01:23 -0300

    Add archivos y modificaciones

---
+++ js/pages/index.js
@@ -0,0 +1,13 @@
+var saludo1 = "hola";
+var saludo2 = "HOLA";
+var saludo3 = "hola";
+
+if(saludo1 === saludo2){
+    console.log("los dos son iguales");
+}
+if(saludo1 !== saludo2){
+    console.log("los dos son diferentes");
+}
+if(saludo1 === saludo3){
+    console.log("1 y 3 son iguales");
+}
